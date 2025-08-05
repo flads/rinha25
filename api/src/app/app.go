54eea198af4c4364fa70d5a8558b9db9b054e22a
flads <flads.me@gmail.com>
2025-08-05 03:24:51 -0300

    feat: add haproxy with unix sockets

--- api/src/app/app.go
+++ api/src/app/app.go
@@ -1,16 +1,18 @@
 package app
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"log"
+	"net"
+	"net/http"
+	"os"
 	"src/datetime"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
-	"github.com/valyala/fasthttp"
 )
 
 type App struct {
@@ -28,37 +30,49 @@ func NewApp() *App {
 }
 
 func (a *App) ListenAndServe() {
-	fasthttp.ListenAndServe(":8080", func(ctx *fasthttp.RequestCtx) {
-		if ctx.IsPost() {
-			switch string(ctx.Path()) {
-			case "/payments":
-				a.payments(ctx)
-				return
-			}
-		}
+	router := gin.Default()
 
-		if ctx.IsGet() {
-			switch string(ctx.Path()) {
-			case "/payments-summary":
-				a.paymentsSummary(ctx)
-				return
-			}
-		}
-
-		ctx.Error("Not found", fasthttp.StatusNotFound)
+	router.POST("/payments", func(ctx *gin.Context) {
+		a.payments(ctx)
 	})
+
+	router.GET("/payments-summary", func(ctx *gin.Context) {
+		a.paymentsSummary(ctx)
+	})
+
+	socketPath := os.Getenv("SOCKET_PATH")
+	if socketPath == "" {
+		socketPath = "/sockets/api-default.sock"
+	}
+
+	if err := os.RemoveAll(socketPath); err != nil {
+		log.Fatal("Erro ao remover socket antigo:", err)
+	}
+
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer listener.Close()
+
+	if err := os.Chmod(socketPath, 0660); err != nil {
+		log.Fatal("Erro ao definir permissões do socket:", err)
+	}
+
+	http.Serve(listener, router)
 }
 
-func (a *App) payments(ctx *fasthttp.RequestCtx) {
+func (a *App) payments(ctx *gin.Context) {
 	var data map[string]interface{}
-	if err := json.NewDecoder(bytes.NewReader(ctx.PostBody())).Decode(&data); err != nil {
-		ctx.Error("Invalid JSON", fasthttp.StatusBadRequest)
+	if err := json.NewDecoder(ctx.Request.Body).Decode(&data); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Error"})
 		return
 	}
 
 	data["requestedAt"] = time.Now().UTC().Format("2006-01-02T15:04:05.000Z")
 
-	ctx.SetStatusCode(fasthttp.StatusNoContent)
+	ctx.Status(http.StatusNoContent)
 
 	go func() {
 		encoded, err := json.Marshal(data)
@@ -74,10 +88,9 @@ func (a *App) payments(ctx *fasthttp.RequestCtx) {
 	}()
 }
 
-func (a *App) paymentsSummary(ctx *fasthttp.RequestCtx) {
-	postArgs := ctx.QueryArgs()
-	from := string(postArgs.Peek("from"))
-	to := string(postArgs.Peek("to"))
+func (a *App) paymentsSummary(c *gin.Context) {
+	from := c.Query("from")
+	to := c.Query("to")
 
 	defaultList := a.getRequests("default_requests", from, to)
 	fallbackList := a.getRequests("fallback_requests", from, to)
@@ -93,8 +106,7 @@ func (a *App) paymentsSummary(ctx *fasthttp.RequestCtx) {
 		},
 	}
 
-	ctx.Response.Header.Set("Content-Type", "application/json")
-	json.NewEncoder(ctx.Response.BodyWriter()).Encode(resp)
+	c.JSON(http.StatusOK, resp)
 }
 
 func (a *App) getRequests(listName string, from string, to string) []string {
@@ -103,11 +115,13 @@ func (a *App) getRequests(listName string, from string, to string) []string {
 	if from != "" && to != "" {
 		fromInt, err1 := datetime.StrToTimeWithMicro(from)
 		toInt, err2 := datetime.StrToTimeWithMicro(to)
+
 		if err1 == nil && err2 == nil {
 			vals, _ := a.client.ZRangeByScore(ctx, listName, &redis.ZRangeBy{
 				Min: fmt.Sprintf("%d", fromInt),
 				Max: fmt.Sprintf("%d", toInt),
 			}).Result()
+
 			return vals
 		}
 	}
@@ -118,6 +132,7 @@ func (a *App) getRequests(listName string, from string, to string) []string {
 
 func (a *App) getRequestsAmountSum(requests []string) float64 {
 	sum := 0.0
+
 	for _, item := range requests {
 		var data map[string]interface{}
 		if err := json.Unmarshal([]byte(item), &data); err == nil {
@@ -126,5 +141,6 @@ func (a *App) getRequestsAmountSum(requests []string) float64 {
 			}
 		}
 	}
+
 	return float64(int(sum*100)) / 100.0
 }
